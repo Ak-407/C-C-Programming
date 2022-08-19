294637b1b38f66a62204dd2aa8397e2866620b65
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2022-08-19 22:00:40 +0530

    Add files via upload

--- Project for student managment.cpp
+++ Project for student managment.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<fstream>
 #include<string>
-
 using namespace std;
 class student{
 	string college; 
@@ -12,10 +11,9 @@ class student{
 	int number;
 	public:
 		int setdata(){
-			
 			ofstream in;
 			in.open("Student Report.txt");
-			
+			cout<<endl;
 			cout<<endl<<"Write College Name = ";
 		    cin>>college;
 		    in<<college<<endl;
@@ -35,7 +33,7 @@ class student{
 			cin>>number;
 			in<<number<<endl;
 		    in.close();
-}
+}         
     	int getdata2(){
     		
     		 string a;
@@ -45,6 +43,66 @@ class student{
     				getline(in, a);
     				cout<<a<<endl;
 					}	
+			}
+			int search(){
+				string x, y, a, b;
+				ifstream in;
+    			in.open("Student Report.txt");
+    		    cout<<"put the word you want to find = ";
+    			cin>>a;
+    			getline(in, x);
+    			
+    			if(a==x /*||b==y ||a==y ||b==x*/){
+	            cout<<"we get it";  
+				}
+				else{
+					cout<<"not found";
+				}
+			}
+			int modify(){
+				
+				int x;
+				cout<<"1.College Name:\n 2.Name:\n 3.Roll number:\n 4.Email ID:\n 5.Coarse\n 6.Phone number:";
+				cin>>x;
+				ofstream in;
+				in.open("Student Report.txt");
+				cout<<endl;
+				switch(x){
+					case 1:
+						cout<<endl<<"Write College Name = ";
+		                cin>>college;
+		                in<<college<<endl;
+		                break;
+		            case 2:
+			            cout<<endl<<"Write Your Name = ";
+			            cin>>name;
+			            in<<name<<endl;
+			            break;
+			        case 3:
+			            cout<<endl<<"Write Roll number = ";
+			            cin>>roll;
+			            in<<roll<<endl;
+			        	break;
+		            case 4:	
+			cout<<endl<<"Write Your Email ID: = ";
+			cin>>email;
+			in<<email<<endl;
+			break;
+			case 5:
+			cout<<endl<<"Write Your coarse = ";
+			cin>>coarse;
+			in<<coarse<<endl;
+			break;
+			case 6:
+			cout<<endl<<"Write Your Phone Number = ";
+			cin>>number;
+			in<<number<<endl;
+			break;
+				
+				}
+				
+			
+		    //in.close(); 
 			}
 			
 };
@@ -77,16 +135,18 @@ int main()
 		 goto menustart;
 	}	
 	else{
-		return 0;
+		return 1;
 	}
 	}
 	else if (a==3){
-		cout<<"Working On This.";
+		//cout<<"Working On This.";
+		c1.modify();
 		 goto menustart;
 	}
 	else if (a==4){
-		cout<<"Working On This.";
-		 goto menustart;
+		//cout<<"Working On This.";
+		c1.search();
+		goto menustart;
 	}
 	else if (a==5){
 		cout<<"Working On This.";
@@ -94,7 +154,7 @@ int main()
 	}
 	else
 	{
-		return 0;
+		return 1;
 	}
 	
    
